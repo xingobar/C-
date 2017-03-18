a071b519e2fe5eab1503ea1c6f2e63557f170984
xingobar <gary840227@gmail.com>
2017-03-18 22:42:13 +0800

    UrlDownloader

---
+++ UrlDownloader/UrlDownloader/UrlDownloader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlDownloader
+{
+    class UrlDownloader
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            UrlToFile(args[0], args[1]);
+        }
+        public static void UrlToFile(String url, String file) {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(url, file);
+        }
+    }
+}
