28da61c34b618db842a45f19b37464ddf178a116
xingobar <gary840227@gmail.com>
2017-03-18 21:23:10 +0800

    Simple Thread Test

---
+++ ThreadTest/ThreadTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(count));
+            Thread thread2 = new Thread(new ThreadStart(count));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.Read();
+        }
+
+        static void count()
+        {
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(i);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
