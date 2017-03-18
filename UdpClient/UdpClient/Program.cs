fa99d938c6aae3ad55b560111eea697a1add32b1
xingobar <gary840227@gmail.com>
2017-03-18 20:58:28 +0800

    Udp execerise

---
+++ UdpClient/UdpClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 連接到 args[0] 參數所指定的 IP 且使用 port 5555
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(args[0]), 5555);
+            // 建立 Socket , 連接到 Internet (InterNetwork) , 使用 UDP 協定的 Datagram (Dgram)
+            // Args1 : InterNetwork
+            // Args2 : Datagram
+            // Args3 : UDP
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // 不斷讀取鍵盤所輸入的資料
+            while (true) {
+                string input = Console.ReadLine(); // 讀取鍵盤輸入
+                if (input == "exit") {
+                    break;
+                }
+                // 將訊息已 UTF8 的方式編碼送出
+                server.SendTo(Encoding.UTF8.GetBytes(input), ipep); 
+            }
+            Console.WriteLine("Stopping client");
+            Console.WriteLine("Closing Connection");
+            server.Close(); // 關閉連線
+        }
+    }
+}
---
+++ UdpServer/UdpServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 開啟伺服器端的 5555 port , 用來接收任何傳送到本機的訊息
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5555);
+            // 建立接收的 Socket , 並使用 Udp 的 Datagram 方式接收
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // 綁定 Socket (socket) 與 IPEndPoint (ipep) , 讓 Socket 接收 5555 port 的訊息
+            socket.Bind(ipep);
+            Console.WriteLine("Waiting for a client .... ");
+            // 建立 Remote 物件以便取得封包的接收來源的 EndPoint 物件
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            // 不斷接收訊息並顯示到螢幕上
+            while (true) {
+                byte[] data = new byte[1024]; // 設定接收緩衝區的陣列變數
+                int recv = socket.ReceiveFrom(data, ref Remote); // 接收對方傳來的封包
+                // 將封包以 UTF8 的格式解碼為字串,並顯示到螢幕上
+                Console.WriteLine(Encoding.UTF8.GetString(data,0,recv));
+            }
+        }
+    }
+}
